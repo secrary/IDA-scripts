94ec2e52310e40367c9642f36d928f96eb5ed8d2
Lasha Khasaia <lkhasaia@outlook.com>
2018-01-11 17:06:48 +0400

    ...

--- XORCheck.py
+++ XORCheck.py
@@ -9,22 +9,23 @@ def main():
 
     while ea < MaxEA():
         ea = FindText(ea, SEARCH_DOWN, 0, 0, "xor")
-        if ea == idc.BADADDR:
+        eCode = idc.isCode(idc.GetFlags(ea))
+        if ea == idc.BADADDR :
             break
         if idc.GetOpnd(ea, 0) == idc.GetOpnd(ea, 1):
             ea = idc.NextHead(ea)
             continue
-            
         func = idaapi.get_func(ea)
         if func.flags & FUNC_LIB:
             ea = idc.NextHead(ea)
             continue
-
-        print "loc: ", hex(ea), "dis: ", idc.GetDisasm(ea), "func: ", idc.get_func_name(ea)
-        
-        idc.MakeComm(ea, "XORs: check it!")
-        
+        if eCode:
+            print "loc: ", hex(ea), "dis: ", idc.GetDisasm(ea), "func: ", idc.get_func_name(ea)
+            idc.MakeComm(ea, "XORs: check it!")
         ea = idc.NextHead(ea)
+
+
+    print "\n--------------- XORCheck EOF --------------\n"
     
         
 main()
