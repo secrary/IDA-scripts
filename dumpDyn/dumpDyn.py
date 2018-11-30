6e897d352d7f43e93779fad17f39447f355456ca
khasaia <khasaia@not_mail_com>
2018-11-30 14:28:19 +0200

    Update dumpDyn.py

--- dumpDyn/dumpDyn.py
+++ dumpDyn/dumpDyn.py
@@ -13,10 +13,32 @@ import ida_name
 import idautils
 import ida_nalt
 import ida_dbg
+import ida_bytes
+import ida_ida
+import ida_kernwin
+import ida_segment
 
 MD5_hash_data_file = None
+SIGNATURE_SIZE = 0x10
 
-def save_x(unique_name, start, size):
+def save_x(unique_name=None, start=None, size=None):
+      
+      ea = ida_kernwin.get_screen_ea()
+
+      # signature
+      if not unique_name:
+            if not start:
+                  seg = ida_segment.getseg(ea)
+                  start = seg.start_ea
+            sig_bytes = ida_bytes.get_bytes(start, SIGNATURE_SIZE)
+            sig_hash = hashlib.md5(sig_bytes).hexdigest()
+            unique_name = sig_hash
+      
+      if not start or not size:
+            seg = ida_segment.getseg(ea)
+            start = seg.start_ea
+            size = seg.size()
+
       # (start_addr, end_addr, names, comms)
       saved_data = {}
       if MD5_hash_data_file and os.path.isfile(MD5_hash_data_file):
@@ -29,7 +51,7 @@ def save_x(unique_name, start, size):
       names_addr_name = []
       names = idautils.Names()
       for addr, name in names:
-            if addr >= start and addr <= start + size:
+            if start <= addr and addr <= start + size:
                   names_addr_name.append((addr - start, name))
 
       # save comments
@@ -58,18 +80,35 @@ def save_x(unique_name, start, size):
                   serial_data = pickle.dumps(saved_data)
                   ifile.write(serial_data)
     
-def restore_x(unique_name, start=here()):
+def restore_x(unique_name=None, start=None):
+
+      ea = ida_kernwin.get_screen_ea()
+
+      # signature
+      if not unique_name:
+            if not start:
+                  seg = ida_segment.getseg(ea)
+                  start = seg.start_ea
+            sig_bytes = ida_bytes.get_bytes(start, SIGNATURE_SIZE)
+            sig_hash = hashlib.md5(sig_bytes).hexdigest()
+            unique_name = sig_hash
+
+      if not start:
+            seg = ida_segment.getseg(ea)
+            start = seg.start_ea
       
       if MD5_hash_data_file and os.path.isfile(MD5_hash_data_file):
             with open(MD5_hash_data_file, "rb") as ifile:
                   received_data = pickle.loads(ifile.read())
                   saved_data = received_data
+                  
                   # (start_addr, end_addr, names, comms, bpts)
                   if unique_name in saved_data:
                         current_data = saved_data[unique_name]
                         
                         # restore names
                         names = current_data[2]
+                        
                         for name in names:
                               # names: (rel_addr, name)
                               ida_name.set_name(start + name[0], name[1])
@@ -97,7 +136,10 @@ def main():
     restore_x(\"unique_name\", start_addr) - restore names, comments, breakpoints\n\
     Example:\n\t\
     save_x(\"first_shellcode\", 0x12340000, 0x1000)\n\t\
-    restore_x(\"first_shellcode\", 0x12340000)\
+    restore_x(\"first_shellcode\", 0x12340000)\n\t\
+    \nBONUS: useful if a process allocated a new segment (e.g. VirtualAlloc) otherwise (HeapAlloc, new, etc.) use the first way\n\t\
+    save_x() == save_x(FIRST_0x10_BYTES_HASH_FROM_EA_SEGMENT, START_OF_EA_SEGMENT, SIZEOF_EA_SEGMENT)\n\t\
+    restore_x() == restore(FIRST_0x10_BYTES_HASH_FROM_EA_SEGMENT, START_OF_EA_SEGMENT)\n\
     ")
     
     global MD5_hash_data_file
@@ -107,6 +149,8 @@ def main():
         buf = afile.read()
         hasher.update(buf)
     MD5_hash = hasher.hexdigest() # str
-    MD5_hash_data_file = input_filepath + "___rstr___" + MD5_hash
+    MD5_hash_data_file = input_filepath + "____rstr___" + MD5_hash
+
   
-main()
+if __name__ == "__main__":
+  main()
