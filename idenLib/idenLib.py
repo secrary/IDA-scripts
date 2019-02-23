aa00acd9aa468b5dc01bc6d54dd3a526cae2061a
Lasha Khasaia <khasaia@>
2019-02-23 13:12:15 +0200

    Update idenLib.py

--- idenLib/idenLib.py
+++ idenLib/idenLib.py
@@ -27,6 +27,7 @@ MIN_FUNC_SIZE = 0x20
 MAX_FUNC_SIZE = 0x100
 PLUGIN_NAME = "idenLib"
 PLUGIN_VERSION = "v0.4"
+IDENLIB_CONTACT = "Contact: Lasha Khasaia (@_qaz_qaz)"
 
 local_appdata = os.getenv('LOCALAPPDATA')
 
@@ -115,6 +116,7 @@ def idenLibProcessSignatures():
         os.mkdir(idenLib_appdata)
     pickle.dump(func_sigs, open( idenLibCache, "wb" ))
     pickle.dump(mainSigs, open( idenLibCacheMain, "wb" ))
+    print("[idenLib] Signatures refreshed...\n")
 
 def idenLib():
     global func_sigs
@@ -193,6 +195,29 @@ class idenLib_class(idaapi.action_handler_t):
     def update(self, ctx):
         return idaapi.AST_ENABLE_FOR_WIDGET if ctx.widget_type == idaapi.BWN_DISASM else idaapi.AST_DISABLE_FOR_WIDGET
 
+class AboutHandler(idaapi.action_handler_t):
+    def __init__(self):
+        idaapi.action_handler_t.__init__(self)
+
+    def activate(self, ctx):
+        print(PLUGIN_NAME + " " + PLUGIN_VERSION)
+        print(IDENLIB_CONTACT)
+        return 1
+
+    def update(self, ctx):
+        return idaapi.AST_ENABLE_ALWAYS
+
+class RefreshHandler(idaapi.action_handler_t):
+    def __init__(self):
+        idaapi.action_handler_t.__init__(self)
+
+    def activate(self, ctx):
+        idenLibProcessSignatures()
+        return 1
+
+    def update(self, ctx):
+        return idaapi.AST_ENABLE_ALWAYS
+
 # icon author: https://www.flaticon.com/authors/freepik
 icon_data = "".join([
     "\x89\x50\x4E\x47\x0D\x0A\x1A\x0A\x00\x00\x00\x0D\x49\x48\x44\x52\x00\x00\x00\x18\x00\x00\x00\x18\x08\x03\x00\x00\x00\xD7\xA9\xCD\xCA\x00\x00\x00\x4E\x50\x4C\x54\x45\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xC4\xA2\xA6\x59\x00\x00\x00\x19\x74\x52\x4E\x53\x00\x20\xEE\x4F\xC9\x64\xD3\xB3\x32\x99\x88\x17\x0C\xC1\x5C\x28\xF6\x7F\xE6\xDD\xBB\xA2\x47\x41\x90\xCE\x19\x07\xA1\x00\x00\x00\xC8\x49\x44\x41\x54\x28\xCF\x75\xD1\xDB\xAE\x83\x20\x10\x85\xE1\x35\x08\x0E\xCA\x16\x3C\xDB\xF5\xFE\x2F\xBA\xC7\x58\xDB\xB4\xA1\xFF\x8D\xC8\x27\x48\x02\x7E\x26\xD6\xDF\xE7\x58\x70\x46\xAB\x79\x82\x23\x19\xD4\x31\x55\xC1\x93\x47\x75\xAB\xFD\x10\xA9\xAE\x38\x16\xEA\x0B\x36\x6F\x6D\x88\x56\x8A\xE4\xFC\x02\xA5\xA5\x58\x9C\x73\x19\x23\x99\x6E\x88\x12\xA3\x94\x6B\x2B\x78\x9B\xB8\xA1\xA5\x9B\xE9\x9F\xF0\x20\xA7\x37\x58\x37\x64\x52\xAB\x50\x48\x57\x85\xF3\x21\x55\x18\x6C\xA6\x0A\x3D\xD9\x1B\x68\x37\x7E\x41\xD3\x4E\x0A\x2C\x40\xF7\x05\x12\x60\x2B\x5C\xC2\x70\x43\x0E\x21\x14\xD8\x97\xD0\x02\x8E\xB3\xFD\xA3\x1D\xD4\x0F\xD0\x75\x5D\x77\x03\x1D\x99\xD1\x5B\x25\xED\x21\x34\x09\x93\x8D\xA3\x41\x9E\xEC\xA5\xB3\xA2\xBF\xB6\x7A\xD8\xF8\x04\xD9\xDA\xA1\x76\x5C\x24\x3A\xBD\x6E\x4D\xCE\xD2\xFB\x36\x05\xBF\xFB\x07\x19\xFC\x16\xA4\x38\xC6\x08\x3D\x00\x00\x00\x00\x49\x45\x4E\x44\xAE\x42\x60\x82"
@@ -201,7 +226,7 @@ icon_data = "".join([
 class idenLibMain(idaapi.plugin_t):
     flags = idaapi.PLUGIN_UNL
     comment = "idenLib - Library Function Identification"
-    help = "Contact: Lasha Khasaia (@_qaz_qaz)"
+    help = IDENLIB_CONTACT
     wanted_name = PLUGIN_NAME
     wanted_hotkey = ''
 
@@ -217,13 +242,44 @@ class idenLibMain(idaapi.plugin_t):
         act_name = "idenLib:action"
         idaapi.register_action(idaapi.action_desc_t(
                 act_name,
-                "idenLib",
+                "idenLib - Function Identification",
                 idenLib_class(),
                 None,
                 "idenLib",
                 act_icon))
         # Insert the action in a toolbar
         idaapi.attach_action_to_toolbar("DebugToolBar", act_name)
+        idaapi.attach_action_to_menu(
+                    'Edit/idenLib/',
+                    act_name,
+                    idaapi.SETMENU_APP)
+
+        # refresh signatures
+        act_name = "idenLib:refresh"
+        idaapi.register_action(idaapi.action_desc_t(
+                act_name,
+                "Refresh Signatures",
+                RefreshHandler(),
+                None,
+                "idenLib - Refresh"))
+        idaapi.attach_action_to_menu(
+                    'Edit/idenLib/',
+                    act_name,
+                    idaapi.SETMENU_APP)
+
+        # about
+        act_name = "idenLib:about"
+        idaapi.register_action(idaapi.action_desc_t(
+                act_name,
+                "About",
+                AboutHandler(),
+                None,
+                "idenLib - About"))
+        idaapi.attach_action_to_menu(
+                    'Edit/idenLib/',
+                    act_name,
+                    idaapi.SETMENU_APP)
+
         return idaapi.PLUGIN_OK
 
 
